f743aee92615d51a2306b448b20e112d213937ce
Raunak Mishra <mishraraunak52@gmail.com>
2025-09-23 10:22:39 +0530

    Count button

--- ReactApp/my-first-app/src/App.jsx
+++ ReactApp/my-first-app/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+ let [state,SetState]= useState(0);
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  function fun1() {
+   SetState(state+1);
+  }
+    return (
+      <div id="main">
+        <label for="name">Name : </label>
+        <input
+          id="name"
+          class="forms"
+          type="text"
+          placeholder="Enter your name"
+        />
+        <br />
+        <label for="email">Email : </label>
+        <input
+          id="email"
+          class="forms"
+          type="email"
+          placeholder="Enter your email"
+        />
+        <br />
+        <label for="password">Password : </label>
+        <input
+          id="password"
+          class="forms"
+          type="password"
+          placeholder="Enter your password"
+        />
+        <br />
+        <input type="submit" class="forms" value="Submit" />
+        <button onClick={fun1}>Click Me</button>
+        <h2>Count : {state}</h2>;
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    );
+  }
+export default App;
